fc7befae73f1ba8f352f6fb1aed574bbac669316
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 19:14:00 +0300

    Ex007 added

---
+++ Ex007/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int a = new Random().Next(10);
+int b = new Random().Next(10);
+int c = new Random().Next(10);
+int d = new Random().Next(10);
+int e = new Random().Next(10);
+int f = new Random().Next(10);
+
+Console.WriteLine(a+","+b+","+c+","+d+","+e+","+f);
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+if (f > max) max = f;
+
+Console.WriteLine("max = " + max);
