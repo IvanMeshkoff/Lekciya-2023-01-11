b437b8bea4616e361bb9b924d7135be8db22a7f5
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 18:57:24 +0300

    Ex006 added

---
+++ Ex006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
