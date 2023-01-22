2ebf12463e32bb800d281608c2efece690b15d34
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 18:14:51 +0300

    Ex004 added

---
+++ Ex004/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+double number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+double number2 = int.Parse(Console.ReadLine());
+double divResult = number1 / number2;
+Console.WriteLine("Разность = " + divResult);
