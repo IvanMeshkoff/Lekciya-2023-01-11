70ee7198a009c80a51aba0c3353304178357c421
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 18:38:37 +0300

    Ex005 added

---
+++ Ex005/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+int number1 = new Random().Next(1, 10); // 1 2 3 ... 9
+Console.WriteLine("Первое случайное число: " + number1);
+int number2 = new Random().Next(-5, 6);
+Console.WriteLine("Второе случайное число: " + number2);
+int result = number1 + number2;
+Console.WriteLine("Сумма случйных чисел = " + result);
