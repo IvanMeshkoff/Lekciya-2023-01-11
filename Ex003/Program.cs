345c75fdcdb9677d0f3b048a086f017670d84bee
Ivan Meshkov <vanek.meshkoff@yandex.ru>
2023-01-22 17:28:13 +0300

    Ex003 added

---
+++ Ex003/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine());
+int result = numberA + numberB;
+Console.Write("Сумма = " + result);
